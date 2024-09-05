83165e3b414def0238ea5f9c217d6060fe6531d4
victor28907 <victor.sieira@polymtl.ca>
2024-09-04 20:20:19 -0400

    Réponse à la question 1 complétée

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+Aire = (math.pi)*rayon**2
+print(Aire)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Victor Sieira")
